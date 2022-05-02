3ec34c2723c17bb1840b7aafd4cc04baf3cfa08c
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-05-02 18:54:35 +0200

    Add style, separate js in script.js, minor fixes

---
+++ script.js
@@ -0,0 +1,31 @@
+/*       const btn1 = document.getElementById("btn1");
+      const btn2 = document.getElementById("btn2");
+
+      function changeColor(color) {
+        document.getElementById("title").style.color = color;
+      }
+      btn1.addEventListener("click", changeColor.bind(null, "red"));
+      btn2.addEventListener("click", changeColor.bind(null, "blue")); */
+
+const form = document.getElementById("form");
+
+function checkHandler(e) {
+  e.preventDefault(); //giusto per non fare richieste a vuoto
+  const matricola = document.getElementById("idNum").value;
+  if (matricola.length !== 12) {
+    alert("Il numero di matricola deve essere composto da 12 cifre.");
+  }
+  if (
+    document.getElementById("tel").value.length == 0 &&
+    document.getElementById("email").value.length == 0
+  ) {
+    alert("Si prega di inserire il numero di telefono o la mail.");
+  }
+  if (document.getElementById("region").value.length == 0) {
+    alert("Si prega di inserire una regione.");
+  }
+}
+form.addEventListener("submit", checkHandler);
+
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = `&copy; Gianluca La Rosa ${year}`;
