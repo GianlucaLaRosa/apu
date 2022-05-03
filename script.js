ee01cfbd70a416ae97c87d76e0b967eea88b308a
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-05-03 15:44:13 +0200

    last fix

--- script.js
+++ script.js
@@ -12,14 +12,31 @@ const form = document.getElementById("form");
 function checkHandler(e) {
   e.preventDefault(); //giusto per non fare richieste a vuoto
   const matricola = document.getElementById("idNum").value;
+  if (document.getElementById("name").value.length == 0) {
+    alert("Si prega di inserire il nome.");
+  }
+  if (document.getElementById("surname").value.length == 0) {
+    alert("Si prega di inserire il cognome.");
+  }
   if (matricola.length !== 12) {
     alert("Il numero di matricola deve essere composto da 12 cifre.");
+  }
+  if (isNaN(+matricola)) {
+    alert("Il numero di matricola deve essere composto solo da cifre.");
   }
   if (
     document.getElementById("tel").value.length == 0 &&
     document.getElementById("email").value.length == 0
   ) {
     alert("Si prega di inserire il numero di telefono o la mail.");
+  }
+  if (
+    document.getElementById("email").value.length > 0 &&
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+      document.getElementById("email").value
+    )
+  ) {
+    alert("Inserire una mail valida.");
   }
   if (document.getElementById("region").value.length == 0) {
     alert("Si prega di inserire una regione.");
