10742b135df19c1f235eb17932b80b8443c0f95d
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-05-04 17:33:36 +0200

    esercizio terzo giorno

--- esercizioExpressJS/app.js
+++ esercizioExpressJS/app.js
@@ -4,12 +4,52 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("users.json");
+let users = JSON.parse(rawdata);
+let isLoggedin = false;
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile("index.html", { root: "." });
 });
 
-app.post("/test", (req, res) => {
-  console.log(req.body);
+app.get("/form", (req, res) => {
+  res.sendFile("form.html", { root: "." });
+});
+
+app.post("/form", (req, res) => {
+  res.sendFile("index.html", { root: "." });
+  let formInputs = {
+    cognome: req.body.surname,
+    nome: req.body.name,
+    piva: req.body.piva,
+  };
+  console.log(formInputs);
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile("login.html", { root: "." });
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.id === users.userName && req.body.password === users.password) {
+    isLoggedin = true;
+    res.sendFile("logged.html", { root: "." });
+  }
+});
+
+app.get("/logged", (req, res) => {
+  if (isLoggedin) {
+    res.sendFile("logged.html", { root: "." });
+  } else {
+    res.sendFile("index.html", { root: "." });
+  }
+});
+
+app.post("/logged", (req, res) => {
+  isLoggedin = false;
+  res.sendFile("index.html", { root: "." });
 });
 
 app.listen(port, () => {
